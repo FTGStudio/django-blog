56d881018cc2707b4f0610a1c1054c8db4a46e97
Michael Doran <doranm@mail.gvsu.edu>
2015-07-28 22:20:11 -0400

    changed template dirs to use SETTINGS PATH

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'blogapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(SETTINGS_PATH, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'blogapp/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
