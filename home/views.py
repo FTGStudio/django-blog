896ddf95d700263e5ba0bc7cfe876d005aec9ac8
Michael Doran <doranm@mail.gvsu.edu>
2015-07-28 21:40:41 -0400

    added a home app and folders to design home page of blogg app.

--- home/views.py
+++ home/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+import traceback
+import sys
+from django.views.generic.base import TemplateView
+
+class HomePageView(TemplateView):
+    try:
+        template_name = "home/index.html"
+
+        def get_context_data(self, **kwargs):
+
+            context = super(HomePageView, self).get_context_data(**kwargs)
+            context['client_list'] = ""
+
+            return context
+    except Exception as inst:
+        traceback.print_exc(file=sys.stdout)
+        raise inst
